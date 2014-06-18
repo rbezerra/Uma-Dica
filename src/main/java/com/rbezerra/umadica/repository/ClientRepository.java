b402544a0b424ba8e114c6d2ebf2f954424194cb
Ramon Bezerra <ramonbezerra90@gmail.com>
2014-06-18 02:17:42 -0300

    removendo arquivos não utilizados

--- src/main/java/com/rbezerra/umadica/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rbezerra.umadica.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.rbezerra.umadica.model.Client;
-
-public interface ClientRepository extends MongoRepository<Client, String> {
-
-}
